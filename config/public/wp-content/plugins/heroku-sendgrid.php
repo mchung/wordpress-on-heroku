27652eaf2a07e8a3402730d9c6476f86076b14d6
Marc Chung <mchung@gmail.com>
2013-08-22 00:05:56 -0700

    Add support for ContactForm7
    If Wordpress sends an email, use default settings, however
    if ContactForm7 sends an email, use the settings defined by
    the contact form.

--- config/public/wp-content/plugins/heroku-sendgrid.php
+++ config/public/wp-content/plugins/heroku-sendgrid.php
@@ -13,15 +13,6 @@ add_action('phpmailer_init', 'heroku_mail_to_sendgrid');
 function heroku_mail_to_sendgrid(&$phpmailer) {
   $phpmailer->IsSMTP();
 
-  $display_name = get_userdata(1)->display_name;
-  $blogname = get_bloginfo('blogname', 'raw');
-  $email = get_bloginfo('admin_email', 'raw');
-
-  $phpmailer->Sender = $email;
-  $phpmailer->From = $email;
-  $phpmailer->FromName = "$display_name from $blogname";
-  $phpmailer->AddReplyTo($email, $admin_name);
-
   $phpmailer->Host = 'smtp.sendgrid.net';
   $phpmailer->Username = getenv("SENDGRID_USERNAME");
   $phpmailer->Password = getenv("SENDGRID_PASSWORD");
@@ -30,4 +21,34 @@ function heroku_mail_to_sendgrid(&$phpmailer) {
   $phpmailer->SMTPAuth = true;
   $phpmailer->SMTPSecure = 'tls';
 }
+
+add_filter('wp_mail_from', 'change_mail_from');
+
+/* Return from email address */
+function change_mail_from($wp_mail_from)
+{
+  return get_bloginfo('admin_email', 'raw');
+}
+
+add_filter('wp_mail_from_name', 'change_mail_from_name');
+
+/* Return mail from name */
+function change_mail_from_name($wp_mail_from_name)
+{
+  $display_name = get_userdata(1)->display_name;
+  $blogname = get_bloginfo('blogname', 'raw');
+  return "$display_name from $blogname";
+}
+
+add_filter('wpcf7_mail_components', 'send_from_wpcf7');
+
+function send_from_wpcf7($components) {
+  // In the case where wpcf7 is called,
+  // depend on those settings instead.
+  remove_filter('wp_mail_from', 'change_mail_from');
+  remove_filter('wp_mail_from_name', 'change_mail_from_name');
+
+  return $components;
+}
+
 ?>
